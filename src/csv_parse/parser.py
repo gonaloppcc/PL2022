6c5e0bfd35946f3f5860b0b1c99747c52c9ed564
Mapc1 <marcocosta579@gmail.com>
2022-03-11 12:36:58 +0000

    Change stdin to file passed as argument

--- src/csv_parse/parser.py
+++ src/csv_parse/parser.py
@@ -1,17 +1,17 @@
-import sys
+from sys import argv
 import re
 
 p = re.compile(r'^(?P<id>\w{24}),(?P<index>\d+),(?P<date>\d{4}-\d{2}-\d{2}),(?P<fname>[^\W\d]+),(?P<lname>[^\W\d]+),(?P<age>\d+),(?P<gender>(M|F)),(?P<location>[^\W\d]+),(?P<sport>\w+),(?P<club>\w+),(?P<email>(\w+((\.|-)\w+)?)+@(\w+((\.|-)\w+)?)+),(?P<federated>(true|false)),(?P<result>(true|false))$')
 
-sys.stdin.readline()
-
 db = []
 
-for line in sys.stdin:
-    mo = p.match(line)
-    dict = mo.groupdict()
+with open(argv[1], mode='r') as file:
+    file.readline()
+    for line in file:
+        mo = p.match(line)
+        dict = mo.groupdict()
 
-    db.append(dict)
+        db.append(dict)
 
-for athlete in db:
-    print(f'{athlete["fname"]} {athlete["lname"]}')
+    for athlete in db:
+        print(f'{athlete["fname"]} {athlete["lname"]}')
