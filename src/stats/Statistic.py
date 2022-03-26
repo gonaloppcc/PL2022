4f5b10c33ca47c602296ecf9f62190871746de6f
Mapc1 <marcocosta579@gmail.com>
2022-03-26 00:04:00 +0000

    Add comments to Statistic.py

--- src/stats/Statistic.py
+++ src/stats/Statistic.py
@@ -3,9 +3,9 @@ from abc import abstractmethod
 
 class Statistic:
     def __init__(self):
-        self._name = 'Stat'
-        self._stats = {}
-        self._data = {}
+        self._name = 'Stat' # Name of the statistic
+        self._stats = {}    # Statistics
+        self._data = {}     # Elements
 
     def set_name(self, name):
         self._name = name
@@ -25,14 +25,17 @@ class Statistic:
     def get_data(self):
         return self._data
 
+    # Adds an element to data and stats
     @abstractmethod
     def add_elem(self, elem):
         pass
 
+    # Prints the data in html format
     @abstractmethod
     def print_data(self):
         pass
 
+    # Prints the stats in html format
     @abstractmethod
     def print_stats(self):
         pass
